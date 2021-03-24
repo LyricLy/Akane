9c841f54d6ca77ea276ad187488de6f80da4b926
Alex Nørgaard <Umbra@AbstractUmbra.xyz>
2021-03-24 13:27:29 +0000

    small edits

--- cogs/meta.py
+++ cogs/meta.py
@@ -157,7 +157,7 @@ class Meta(commands.Cog):
         await self.bot.set_guild_prefixes(ctx.guild, [])
         await ctx.send(ctx.tick(True))
 
-    @commands.command(enabled=False)
+    @commands.command()
     async def source(self, ctx, *, command: str = None):
         """Displays my full source code or for a specific command.
 
@@ -166,7 +166,7 @@ class Meta(commands.Cog):
         or by spaces.
         """
         source_url = "https://github.com/AbstractUmbra/Akane"
-        branch = "master"
+        branch = "main"
         if command is None:
             return await ctx.send(source_url)
 
